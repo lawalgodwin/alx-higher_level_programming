e6560a382716cf76e00c2034e4fbd424cc2a0c81
Lawal Godwin <godwinl200@gmail.com>
2022-12-01 06:21:53 -0800

    Print the number of and the list of arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from sys import argv, exit
+
+
+def main():
+    """Prints the number of and the list of its arguments.
+
+    Returns:
+        0 on success 1 otherwise
+
+    """
+    if ((len(argv) - 1) == 0):
+
+        print("0 arguments.")
+
+    elif ((len(argv) - 1) == 1):
+
+        print("1 argument:")
+
+        print("{0} {1}".format((len(argv) - 1), argv[1]))
+
+    else:
+
+        print("{} arguments:".format(len(argv) - 1))
+
+        for arg in range((len(argv) - 1)):
+
+            print("{0} {1}".format((arg + 1), argv[arg + 1]))
+
+    return (0)
+
+
+if __name__ == '__main__':
+    exit(main())
