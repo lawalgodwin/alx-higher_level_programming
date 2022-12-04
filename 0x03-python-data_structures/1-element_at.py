787c8e1ebb23e3f0422c1fb2c1962792c8acaaaf
Lawal Godwin <godwinl200@gmail.com>
2022-12-04 01:51:40 -0800

    Retrieve an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+def element_at(my_list, idx):
+    """A function that retrieves an element from a list
+    Args:
+        my_list: the list
+        idx: the index of the element
+    Returns:
+        returns the element if found otherwise None
+    """
+    num_of_elements = len(my_list)
+
+    if (idx < 0 or idx >= num_of_elements):
+        return
+    return my_list[idx]
