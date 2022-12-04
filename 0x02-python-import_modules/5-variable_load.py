2ac09beb74be2f2507b0324e6a26d72bc63bf6da
Lawal Godwin <godwinl200@gmail.com>
2022-12-04 15:06:04 -0800

    Import a single variable from a file

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def main():
+    """Import a variable from a file
+    Returns: None
+    """
+    from variable_load_5 import a
+    
+    print(a)
+
+if __name__ == '__main__':
+    main()
