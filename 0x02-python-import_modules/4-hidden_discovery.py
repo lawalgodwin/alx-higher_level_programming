918b03e097b70a5eec4069aac4c1e9aaa2376d7d
Lawal Godwin <godwinl200@gmail.com>
2022-12-06 03:54:04 -0800

    Get all named variables and fuctions in an imported module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+def main():
+    """Print all the names defined by the compiled module hidden_4.py
+    Returns:
+        returns None
+    """
+    names = dir(hidden_4)
+
+    for n in names:
+
+        if not(n.startswith("__")):
+
+            print(n)
+
+
+if __name__ == '__main__':
+
+    main()
