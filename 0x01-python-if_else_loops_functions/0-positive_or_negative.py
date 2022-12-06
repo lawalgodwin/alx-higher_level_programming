f631a914a9d2d0ada69802fc5ccd548427864872
Lawal Godwin <godwinl200@gmail.com>
2022-12-06 05:48:12 -0800

    Test for sign of a random number

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -7,11 +7,11 @@ number = random.randint(-10, 10)
 
 def main():
 
-    if number.__lt__(0):
+    if number < 0:
         print("{} is negative".format(number))
         return (0)
 
-    elif number.__eq__(0):
+    elif number == 0:
         print("{} is zero".format(number))
         return (0)
     print("{} is positive".format(number))
