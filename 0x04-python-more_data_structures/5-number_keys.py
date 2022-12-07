6cb7912bf41e9645dc982f5efdf156ae32b58881
Lawal Godwin <godwinl200@gmail.com>
2022-12-07 06:37:06 -0800

    Get the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """Get the Number of keys in a dictonary
+
+    Args:
+        a_dictionary: The dictionary
+
+    Returns:
+        Returns the number of keys in a dictionary
+    """
+
+    return len(list(a_dictionary))
+
+
+if __name__ == '__main__':
+
+    number_keys()
