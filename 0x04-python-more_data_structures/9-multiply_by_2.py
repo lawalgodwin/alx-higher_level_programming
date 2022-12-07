2a0d3f95d0b5f590b18f9ac2404aadcf37e1707a
Lawal Godwin <godwinl200@gmail.com>
2022-12-07 08:32:21 -0800

    Multiply all values in a dictionary by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
+    """Multiply the values in a dictionary by 2
+
+    Args:
+        a_dictionary: the dictionary
+
+    Returns:
+        a new dictionary containing the multiplication result
+
+    """
 
     new_dict = {k: 2 * v for (k, v) in a_dictionary.items()}
 
