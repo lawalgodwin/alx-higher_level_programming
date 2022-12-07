ea8fb0f853798ada1a66726840ab5b2684d35ead
Lawal Godwin <godwinl200@gmail.com>
2022-12-07 06:03:53 -0800

    Get elements common to 2 sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """Get only the element(s) common to two sets
+
+    Args:
+        set_1: The fisrt set
+        set_2: The second set
+
+    Returns:
+        Returns a list containing only the common element
+    """
+
+    return (set_1 & set_2)
+
+
+if __name__ == '__main__':
+
+    common_elements()
