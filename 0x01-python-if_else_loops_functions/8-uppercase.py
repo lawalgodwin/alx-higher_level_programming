18abefee1cc7c200fef492948b53d474176a76eb
lawalgodwin <godwinl200@gmail.com>
2022-12-11 17:52:20 +0000

    Print all in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def uppercase(str1):
+    """Print a string in uppercase
+
+    Args:
+        str1: The string
+
+    Returns:
+        None
+    """
+
+    for s in str1:
+    
+        if (ord(s) >= 97) and (ord(s) <= 122):
+
+            s = chr(ord(s) - 32)
+
+        print("{}".format(s), end='')
+
+    print()
+    
