52bb38b3a9ad6e894e27fa1a1a29e418ad130ab2
lawalgodwin <godwinl200@gmail.com>
2022-12-11 13:40:45 +0000

    Print the last digit of a nf a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ result = (num % 10, number) if number >= 0 else ((num % 10) * (-1), number)
 
 if (result[0] < 6 and result[0] != 0):
 
-    print(f"Last digit of {result[1]:d} is {result[0]:d} and is less than 6 and not 0")
+    print(f"Last digit of {result[1]} is {result[0]} and is less than 6 and not 0")
 
 elif (result[0] > 5):
 
