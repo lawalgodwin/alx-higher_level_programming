4c58bf99469faa8bbade2afce1e58f25ea73da0c
lawalgodwin <godwinl200@gmail.com>
2022-12-19 23:19:57 +0000

    Raise name error with message from a function

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    """Raise name error with message within a function"""
+    """Raise name error with message within a function
+    Args:
+        message: The message
+    """
     raise NameError(message)
