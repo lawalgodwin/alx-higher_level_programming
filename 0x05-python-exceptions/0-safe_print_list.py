15ad30631d17b6ea7a5cc6a407205d82593baa7b
lawalgodwin <godwinl200@gmail.com>
2022-12-19 10:53:33 +0000

    Print a specific number of elements in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """Print a specific number of elements in a list
+    Args:
+        my_list: The list
+        x: The number of numbers to be printed
+
+    Returns:
+    The actual number of elements printed
+    """
 
     count = 0
     
