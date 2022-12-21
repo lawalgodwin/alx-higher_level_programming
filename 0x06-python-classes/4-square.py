ad0b7c2071752adaf3895befc715d388c9da1446
lawalgodwin <godwinl200@gmail.com>
2022-12-21 08:36:04 +0000

    Add getter and setter of an attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- 4-square.py created by Godwin Lawal -*-
+
+"""Defines a Square"""
+
+
+class Square:
+    """Create the blueprint of a Square object."""
+
+    def __init__(self, size=0):
+        """Initialize an instance of a Square
+
+        Args:
+            size: The specified size of the square to be created
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+
+    def area(self):
+        """Get the area of a square object from a public method
+        Returns:
+            The calculated area
+        """
+
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Getter  method for the size attribute
+
+        Returns:
+            The size of the created square object
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        """Setter method for the size attribute
+
+        Changes the existing size to a specified new one
+
+        Args:
+            size: the new size
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
