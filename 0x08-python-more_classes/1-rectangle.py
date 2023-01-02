949857abdb928e6fd4a3e1835e1ea2da396cace6
lawalgodwin <godwinl200@gmail.com>
2023-01-02 18:06:00 +0000

    Add initialiser, getter and setter for width and height instance attribute

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+""" Rectangle Module """
+
+
+class Rectangle:
+    """The blueprint for constucting a Rectangle object"""
+    pass
+
+    def __init__(self, width=0, height=0):
+        """ Initialise a rectangle object """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ Getter of the width instance attribute """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ Setter of the width instance attribute """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter of the height instance attribute """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ Setter of the height instance attribute """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__height = value
