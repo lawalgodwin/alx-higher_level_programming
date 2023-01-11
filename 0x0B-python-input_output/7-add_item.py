02e34bea7e54005c023e4f97c6c5a07c777afb97
lawalgodwin <godwinl200@gmail.com>
2023-01-11 17:06:50 +0100

    Add load and save object to/from file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,19 +4,18 @@
 
 from sys import argv
 
-old_list = []
-
-new_list = []
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-if len(load_from_json_file('add_item.json')):
+try:
 
-    old_list = load_from_json_file('add_item.json')
+    new_list = load_from_json_file('add_item.json')
 
-new_list = [*old_list, *argv[1:]]
+except FileNotFoundError:
 
+    new_list = []
+
+new_list += argv[1:]
 save_to_json_file(new_list, 'add_item.json')
 
 print(load_from_json_file('add_item.json'))
