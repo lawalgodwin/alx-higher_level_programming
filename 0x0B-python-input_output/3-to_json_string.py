4023a7701117e174b5b3c2723ad0fa22a3cda815
lawalgodwin <godwinl200@gmail.com>
2023-01-11 12:06:52 +0100

    Convert dictionary to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""dictionary to json string module"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Convert dictionary to json string"""
+
+    return json.dumps(my_obj)
