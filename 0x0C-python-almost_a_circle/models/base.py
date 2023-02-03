144a63793fc0d0475b47249fd535885adc46d985
lawalgodwin <godwinl200@gmail.com>
2023-02-03 19:15:05 +0100

    Add solution to task 0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""The base class for all classes"""
+
+
+class Base:
+    """The base class blueprint"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The base class constructor"""
+        if id is not None:
+
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
