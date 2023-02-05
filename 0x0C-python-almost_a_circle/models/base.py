7c0f8404d32a27b30623a674c0d8ba2707f74683
lawalgodwin <godwinl200@gmail.com>
2023-02-05 16:56:14 +0100

    Save object to Json file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,22 @@ class Base:
         if not all((isinstance(i, dict) for i in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save to a json file the list of objects"""
+
+        list_dict = []
+
+        filename = f"{cls.__name__}.json"
+
+        with open(filename, 'w', encoding='utf-8') as fd:
+
+            if list_objs is None or len(list_objs) == 0 or list_objs == []:
+                fd.write('[]')
+
+            else:
+                list_dict = [obj.to_dictionary() for obj in list_objs]
+                json_string = Base.to_json_string(list_dict)
+
+                fd.write(json_string)
