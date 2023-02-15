2220e484bab8c2742be9e68a675d153fc75d05b5
lawalgodwin <godwinl200@gmail.com>
2023-02-15 18:56:59 +0100

    Group records by column

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,13 @@
+-- A script that lists the number of records and group them by a column
+--
+--  The result should display:
+--    the score
+--    the number of records for this score with the label number
+--  The list should be sorted by the number of records (descending)
+--  The database name will be passed as an argument to the mysql command
+--
+-- List the number of rows with the same score in the table `second_taable`
+
+SELECT score, COUNT(score) AS "number"
+FROM second_table
+GROUP BY score
