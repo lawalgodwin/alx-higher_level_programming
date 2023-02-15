a47aabe46e40b4fbe6de5aba1e7896c4b30f1138
lawalgodwin <godwinl200@gmail.com>
2023-02-15 15:41:45 +0100

    Show full description(structure) of a table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,8 @@
+-- A script that prints the full description of the table
+
+-- The database name will be passed as an argument of the mysql command
+-- Print the full description of the table `first_table`
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+
+
+SHOW CREATE TABLE first_table
