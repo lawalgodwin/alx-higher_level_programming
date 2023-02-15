58e92cf611f2c6a972d950dc7b2447254c86a3e1
lawalgodwin <godwinl200@gmail.com>
2023-02-15 17:34:28 +0100

    Get result of a query in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,14 @@
+-- A script that orderly lists all records of selected columns in the table
+--
+-- Results should display both the score and the name (in this order)
+--
+-- Records should be ordered by score (top first)
+--
+-- The database name will be passed as an argument of the mysql command
+--
+-- Get the names and score then order the resulting table in descending
+-- order of score
+
+SELECT score, name
+FROM second_table
+ORDER BY score;
