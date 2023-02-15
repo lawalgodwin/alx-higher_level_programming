279242231a03e1d2abc0444330680d546acf046f
lawalgodwin <godwinl200@gmail.com>
2023-02-15 18:16:53 +0100

    Add how to use the script

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,8 @@
 -- A script that lists all databases in a server
-
+--
+-- Usage:
+--    cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
+--
 -- list all databases on this server
 
 SHOW DATABASES
