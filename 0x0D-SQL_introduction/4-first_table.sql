5f935e45743e8ee042f960d118bbb7ed51682c27
lawalgodwin <godwinl200@gmail.com>
2023-02-15 15:31:55 +0100

    Create a table in the current DB

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,10 @@
+-- A script that creates a table in the current DB
+
+-- Create a table `first_table`
+-- with id INT and name VARCHAR(256) attributes
+
+CREATE TABLE IF NOT EXISTS first_table
+    (
+     id INT,
+     name VARCHAR(256)
+    );
