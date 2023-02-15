bd0824c5eb5be59b2fe71cc938fbe5931c1bdcd2
lawalgodwin <godwinl200@gmail.com>
2023-02-15 16:30:51 +0100

    List all rows from a table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,8 @@
+-- A script that lists all rows of a table in a DB
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
+
+-- List all rows of the table `first_table` from the database `hbtn_0c_0`
+-- in your MySQL server
+
+SELECT * FROM first_table;
