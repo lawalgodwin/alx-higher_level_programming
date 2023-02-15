a7a3ec44f73ed87c857a420e9131abfe3b6e91bb
lawalgodwin <godwinl200@gmail.com>
2023-02-15 18:38:51 +0100

    Compute the average of all records

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -2,7 +2,7 @@
 --
 -- The result column name should be average
 -- The database name will be passed as an argument of the mysql command
-
+--
 -- Compute the score average of all records in the table `second_table`
 
-SELECT AVG(*) AS average from second_table
+SELECT AVG(*) AS 'average' from second_table
