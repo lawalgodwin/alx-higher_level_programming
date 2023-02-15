bc543e0eb82fbb1437dcb35a6032841c567de29b
lawalgodwin <godwinl200@gmail.com>
2023-02-15 18:21:41 +0100

    Add how to use the script

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,9 @@
 -- A script that lists all tables in a mysql database
-
+--
+-- Usage: The database name will be passed as argument of mysql command
+-- (in the following example: mysql is the name of the database)
+-- cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
+--
 -- list all tables in a database
 
 SHOW TABLES
