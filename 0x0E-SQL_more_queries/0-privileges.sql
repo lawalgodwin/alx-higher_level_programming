d7d2b72c7e2ca7b976faf2ad616f4f99a7274602
lawalgodwin <godwinl200@gmail.com>
2023-02-16 16:48:35 +0100

    List the privileges of a mysql server user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,7 @@
 -- Lists all privileges of the users accessing the MYSQL server hosted locally or remotely
 --
 -- List all privileges for user_0d_1 on the localhost
-SHOW GRANTS FOR user_0d_1@localhost
+SHOW GRANTS FOR user_0d_1@localhost;
 --
 -- List all privilege for usER_0d_2 on localhost
 SHOW GRANTS FOR user_0d_2@localhost
