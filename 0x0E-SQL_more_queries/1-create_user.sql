42ba476e2e48f01b443cd834ebe5ce6ce9633612
lawalgodwin <godwinl200@gmail.com>
2023-02-16 17:05:37 +0100

    Create a mysql server user and assign all privileges to the user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -10,4 +10,4 @@ CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED WITH 'user_0d_1_pwd';
 
 -- Grant all privileges to the user just created
 
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
