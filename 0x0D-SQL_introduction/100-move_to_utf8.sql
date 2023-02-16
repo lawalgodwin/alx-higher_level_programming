48faaceb6838b0da8a0924ed0dd9d190ebb6874d
lawalgodwin <godwinl200@gmail.com>
2023-02-16 10:43:12 +0100

    Convert Database characterset encoding to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- A script that converts Database, Table and Column to `UTF8`
+--
+-- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+--
+-- switch to the database `hbtn_0c_0`
+USE hbtn_0c_0
+--
+-- Converts first_table table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER TABLE first_table
+CONVERT TO
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
