9158f97ff02cdc43be94c91184edecd6e5a37531
lawalgodwin <godwinl200@gmail.com>
2023-02-16 11:00:57 +0100

    Add script usage instruction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- A script that converts Database, Table and Column to `UTF8`
+-- Usage:
+--   cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p 
 --
 -- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0
