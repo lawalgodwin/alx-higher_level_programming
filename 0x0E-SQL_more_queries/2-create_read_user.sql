9789b8a0d1370f397ca095621695094c49909a41
lawalgodwin <godwinl200@gmail.com>
2023-02-17 12:52:38 +0100

    Create read-only user for a database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,7 +7,7 @@ USE hbtn_0d_2;
 
 -- Create a user `user_0d_2` for the mysql server with the password `user_0d_2_pwd`
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user__0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 --
 -- Grant only SELECT privilege to the user
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
