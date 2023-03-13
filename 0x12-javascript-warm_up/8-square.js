a8d3e6c9a84690c9ec8f1b94a6e37849b8570f5c
lawalgodwin <godwinl200@gmail.com>
2023-03-13 17:42:35 +0100

    Print a square to the screen

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+let row = '';
+const printChar = 'x';
+
+if (!firstArg || isNaN(firstArg)) console.log('Missing size');
+
+else {
+  for (let i = 0; i < firstArg; i++) {
+    row += printChar;
+  }
+  for (let j = 0; j < firstArg; j++) console.log(row);
+}
