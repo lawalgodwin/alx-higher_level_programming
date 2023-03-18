ac4c3f321a893908f4694245ec9dd3c0b23e41ff
lawalgodwin <godwinl200@gmail.com>
2023-03-18 21:08:34 +0100

    Filter out states staring with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
 
     query = '''SELECT * FROM states
                WHERE name LIKE "N%"
+               ORDER BY states.id ASC
             '''
     db_cursor.execute(query)
 
