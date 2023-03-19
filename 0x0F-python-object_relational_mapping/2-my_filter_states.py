b42eb71397955304bc33b08bdeea1dcea86d100d
lawalgodwin <godwinl200@gmail.com>
2023-03-19 17:48:19 +0100

    Get all sttes that matches an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""A script that takes in an argument and
+   displays all values in the states table
+   of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+
+from sys import argv
+
+if __name__ == '__main__':
+    """List states matching the argument"""
+    # connect to DB and get cursur
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    DB = MySQLdb.connect(user=user, passwd=password, db=database)
+
+    db_cursor = DB.cursor()
+    # query the DB usng the cursor
+    stmt = 'SELECT * FROM states WHERE name = "{}"'.format(argv[4])
+    db_cursor.execute(stmt)
+
+    rows_fetched = db_cursor.fetchall()
+    for row in rows_fetched:
+        print(row)
