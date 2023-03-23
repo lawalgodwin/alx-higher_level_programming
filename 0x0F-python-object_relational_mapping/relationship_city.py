0a188da35a44fa3c7a3b3d5af933559505532fdb
lawalgodwin <godwinl200@gmail.com>
2023-03-23 19:22:35 +0100

    Add two dependent classes

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Maping City class to cities table"""
+from model_state import Base, State
+from sqlalchemy import (
+                    Column,
+                    Integer,
+                    String,
+                    ForeignKey)
+
+
+class City(Base):
+    """The city blueprint to be mapped"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Mapping the `State` class to DB table `states`"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (Integer, String, Column)
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """The class to be mapped to states table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref='state', cascade='all, delete')
