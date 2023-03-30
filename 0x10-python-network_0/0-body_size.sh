66d98972ec60fcfeda82c8526406f69ffa66da18
lawalgodwin <godwinl200@gmail.com>
2023-03-30 08:56:37 -0700

    Get the size of the HTTP Response body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that 
-# URL, and displays the size of the body of the response
-
-if [[ $# -lt 1 ]]; then
-  exit 1
-fi
+# A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
 URL=$1
 
-curl -sI "$URL" | wc -c
+curl -sI "$URL" | grep -i "Content-Length" | grep -Eo "[0-9]+"
