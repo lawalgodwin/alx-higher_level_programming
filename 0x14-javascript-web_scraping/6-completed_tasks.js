6d186f87e1f70d9a32a2148ed9d6da3a2deaff17
lawalgodwin <godwinl200@gmail.com>
2023-04-26 18:25:05 +0100

    Sort all completed todos from jsonplaceholder API

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,14 +5,10 @@ const request = require('request');
 
 request(`${process.argv[2]}`, { json: true }, (err, res, body) => {
   if (err) return console.error(err);
-  const completedTasks = {};
-  const completed = body.filter((task) => task.completed == true)
+  const completed = body.filter((task) => task.completed === true);
   const completedTask = {};
   completed.forEach(task => {
-    if (!completedTask[task.userId])
-      completedTask[task.userId] = 1;
-    else
-      completedTask[task.userId] += 1;
-  })
+    if (!completedTask[task.userId]) { completedTask[task.userId] = 1; } else { completedTask[task.userId] += 1; }
+  });
   console.log(completedTask);
 });
