f0c5bfebaeb18352efb85222d4cd0f4a80e3b0e0
lawalgodwin <godwinl200@gmail.com>
2023-04-26 18:19:36 +0100

    Sort all completed todos from jsonplaceholder API

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// A script that computes the number of tasks completed by user id
+
+const request = require('request');
+
+request(`${process.argv[2]}`, { json: true }, (err, res, body) => {
+  if (err) return console.error(err);
+  const completedTasks = {};
+  const completed = body.filter((task) => task.completed == true)
+  const completedTask = {};
+  completed.forEach(task => {
+    if (!completedTask[task.userId])
+      completedTask[task.userId] = 1;
+    else
+      completedTask[task.userId] += 1;
+  })
+  console.log(completedTask);
+});
