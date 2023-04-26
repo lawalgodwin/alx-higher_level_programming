7349623f26876ae0f772d264292107df4841999c
lawalgodwin <godwinl200@gmail.com>
2023-04-26 18:42:50 +0100

    Save the content of a page in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,5 +6,7 @@ const fs = require('fs');
 
 request(`${process.argv[2]}`, (err, res, body) => {
   if (err) return console.error(err);
-  fs.writeFile(`${process.argv[3]}`, body, 'utf-8', (err) => console.error(err))
-})
+  fs.writeFile(`${process.argv[3]}`, body, 'utf8', (err) => {
+    if (err) console.error(err);
+  });
+});
