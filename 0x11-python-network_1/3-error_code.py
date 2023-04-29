d63cc558768195d92af497dd8f50f898b97ef5e2
lawalgodwin <godwinl200@gmail.com>
2023-04-29 11:52:46 +0100

    Handle HTTP ERROR code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,8 +5,3 @@ from sys import argv
 from urllib import request, error
 
 url = argv[1]
-try:
-    with request.urlopen(url) as res:
-        print(res.read().decode('utf-8'))
-except error.HTTPError as e:
-    print("Error code: {}".format(e.code))
