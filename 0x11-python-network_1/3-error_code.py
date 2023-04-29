268bd970093af10822b229c6f1ab8fb9f6029d3c
lawalgodwin <godwinl200@gmail.com>
2023-04-29 12:00:41 +0100

    Handle HTTP ERROR code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,3 +5,16 @@ from sys import argv
 from urllib import request, error
 
 url = argv[1]
+
+
+def make_get_request():
+    """Make a get request to the server"""
+    with request.urlopen(url) as res:
+        print(res.read().decode('utf-8'))
+
+
+if __name__ == '__main__':
+    try:
+        make_get_request()
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
