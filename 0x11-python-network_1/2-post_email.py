43b3b3a1bcd9614fe178497a5b6782585916033a
lawalgodwin <godwinl200@gmail.com>
2023-04-29 10:00:16 +0100

    Post data to a URL

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,12 +6,12 @@ import urllib.parse
 import urllib.request
 
 url = sys.argv[1]
-data = {'email': sys.argv[2]}
+data = {"email": sys.argv[2]}
 
 data = urllib.parse.urlencode(data)
-data = data.encode('utf-8')
+data = data.encode("utf-8")
 
 req = urllib.request.Request(url, data)
 
 with urllib.request.urlopen(req) as res:
-    print(res.read().decode('utf-8'))
+    print(res.read().decode("utf-8"))
