154e3fccb820c1fd5a9b4717c2ffbcffe994d81d
lawalgodwin <godwinl200@gmail.com>
2023-04-29 09:45:13 +0100

    Post data to a URL

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A Python script that sends data (POST Request)"""
+
+import sys
+import urllib.parse
+import urllib.request
+
+url = sys.argv[1]
+data = dict()
+data['email'] = sys.argv[2]
+
+data = urllib.parse.urlencode(data)
+data = data.encode('ascii')
+
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as res:
+    print("{}".format(res.read().decode('utf-8')))
