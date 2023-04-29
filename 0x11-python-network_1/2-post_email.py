845ade8ab88d6be9f8663936b11ae560727cfd26
lawalgodwin <godwinl200@gmail.com>
2023-04-29 10:15:59 +0100

    Post data to a URL

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """ A Python script that sends data to a url [POST Request]
 Usage: ./2-post_email.py <URL> <email>
-Display the uft-8 decoder response
+Display the uft-8 decoded response
 """
 
 import sys
 import urllib.parse
-import urllib.request
+import urllib.requesti
 
-url = sys.argv[1]
-raw_data = {"email": sys.argv[2]}
+def post_data():
+    """Post data to server"""
+    url = sys.argv[1]
+    raw_data = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(raw_data).encode("utf-8")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
 
-data = urllib.parse.urlencode(raw_data).encode("ascii")
-req = urllib.request.Request(url, data)
-
-with urllib.request.urlopen(req) as res:
-    print(res.read().decode("utf-8"))
+if __name__ == '__main__':
+    post_data()
