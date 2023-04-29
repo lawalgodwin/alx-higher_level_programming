d9770a03bcd37a19d0e49a54ee7beb214e089eac
lawalgodwin <godwinl200@gmail.com>
2023-04-29 14:05:08 +0100

    Send data using 'requests' module to server

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Send data to server using the requests module"""
+
+from sys import argv
+import requests
+
+url = argv[1]
+raw_data = argv[2]
+
+if __name__ == '__main__':
+
+    res = requests.post(url, data={'email': raw_data})
+    print(res.content)
