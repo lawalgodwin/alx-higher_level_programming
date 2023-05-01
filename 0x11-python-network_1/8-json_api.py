a70be1aed527f5c4ea25e6110f28f0188a0ea13a
lawalgodwin <godwinl200@gmail.com>
2023-05-01 13:26:29 +0100

    Send payload

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Send payload to server using the requests module 'get' method"""
+"""Send payload to server using the requests module"""
 from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    data = argv[1] if argv[2] else ""
+    data = argv[1] if len(argv) == 2 else ""
     payload = {"q": data}
     url = 'http://0.0.0.0:5000/search_user'
 
@@ -14,8 +14,7 @@ if __name__ == "__main__":
 
     if response == {}:
         print('No result')
-        return
-    if not response:
+    elif not response:
         print('Not a valid JSON')
-        return
-    print("[{}] {}".format(res.json().get('id'), res.json().get('name')))
+    else:
+        print("[{}] {}".format(res.json().get('id'), res.json().get('name')))
